0811b62d7c3c1ae5159739c24694edc2b364fab7
babuji8 <73380497+babuji8@users.noreply.github.com>
2021-01-08 22:09:23 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -15,6 +15,19 @@ function setup() {
 	createCanvas(800, 700);
 	rectMode(CENTER);
 	
+	boxPosition=width/2-100 boxY=610;
+	boxleftSprite=createSprite(boxPosition, boxY, 20,100);
+	boxleftSprite.shapeColor=color(255,0,0);
+	boxLeftBody = Bodies.rectangle(boxPosition+20, boxY, 20,100 , {isStatic:true} ); World.add(world, boxLeftBody);
+	boxBase=createSprite(boxPosition+100, boxY+40, 200,20);
+	boxBase.shapeColor=color(255,0,0);
+	boxBottomBody = Bodies.rectangle(boxPosition+100, boxY+45-20, 200,20 , {isStatic:true} );
+	World.add(world, boxBottomBody);
+	boxleftSprite=createSprite(boxPosition+200 , boxY, 20,100);
+	boxleftSprite.shapeColor=color(255,0,0);
+	boxRightBody = Bodies.rectangle(boxPosition+200-20 , boxY, 20,100 , {isStatic:true} );
+	World.add(world, boxRightBody);
+	
 
 	packageSprite=createSprite(width/2, 80, 10,10);
 	packageSprite.addImage(packageIMG)
